24a0f4377204c6e602226205ebfdec4af85bf199
Ankit sharma <unknownsdeveloper@gmail.com>
2017-06-06 18:18:35 +0530

    Initial commit

---
+++ app/src/main/java/ankit/developer/unknowns/circularlayoutmanager/Model.java
@@ -0,0 +1,26 @@
+package ankit.developer.unknowns.circularlayoutmanager;
+
+/**
+ * Dummy list item model.
+ */
+
+class Model {
+
+    private String event, timings;
+
+    String getEvent() {
+        return event;
+    }
+
+    void setEvent(String event) {
+        this.event = event;
+    }
+
+    String getTimings() {
+        return timings;
+    }
+
+    void setTimings(String timings) {
+        this.timings = timings;
+    }
+}
